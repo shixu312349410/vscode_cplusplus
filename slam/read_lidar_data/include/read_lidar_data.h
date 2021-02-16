d1ef0a90723182939c2184d454a964bdca2e5d8a
xshian <312349410@qq.com>
2021-02-17 03:26:29 +0800

    add read_point_cloud

--- slam/read_lidar_data/include/read_lidar_data.h
+++ slam/read_lidar_data/include/read_lidar_data.h
@@ -6,4 +6,9 @@ struct vector3d
 {
     /* data */
     float x,y,z;
+    
+    vector3d():x(0), y(0), z(0){}  
+    vector3d(float x1, float y1, float z1):x(x1), y(y1), z(z1){}  
+    vector3d(const vector3d &v);  
+    ~vector3d();  
 };
--- slam/read_lidar_data/src/read_lidar_data.cpp
+++ slam/read_lidar_data/src/read_lidar_data.cpp
@@ -12,7 +12,8 @@ std::vector<float> read_lidar_data(const std::string lidar_data_path)
     return lidar_data_buffer;
 }
 
-void read_lidar_point_cloud(const std::string lidar_data_path){
+//通过传递引用的方式获得点云数据
+void read_lidar_point_cloud(const std::string lidar_data_path,vector<vector3d>& lidar_points){
     
     // std::string dataset_folder, sequence_number, output_bag_file;
     // std::stringstream lidar_data_path;
@@ -21,45 +22,22 @@ void read_lidar_point_cloud(const std::string lidar_data_path){
     //std::vector<float> lidar_data = read_lidar_data(lidar_data_path.str());
     std::vector<float> lidar_data = read_lidar_data(lidar_data_path);
     std::cout << "totally " << lidar_data.size() / 4.0 << " points in this lidar frame \n";
-    std::vector<vector3d> lidar_points;
+    //std::vector<vector3d> lidar_points;
     std::vector<float> lidar_intensities;
     for (std::size_t i = 0; i < lidar_data.size(); i += 4)
     {
         lidar_points.emplace_back(lidar_data[i], lidar_data[i+1], lidar_data[i+2]);
         lidar_intensities.push_back(lidar_data[i+3]);
         
-        point.x = lidar_data[i];
-        point.y = lidar_data[i + 1];
-        point.z = lidar_data[i + 2];
-        point.intensity = lidar_data[i + 3];
-        laser_cloud.push_back(point);
-    }        
+        // point.x = lidar_data[i];
+        // point.y = lidar_data[i + 1];
+        // point.z = lidar_data[i + 2];
+        // point.intensity = lidar_data[i + 3];
+        // laser_cloud.push_back(point);
+    }
+    //return lidar_points;        
 
 }
-
-
-// // read lidar point cloud
-//         std::stringstream lidar_data_path;
-//         lidar_data_path << dataset_folder << "sequences/" + sequence_number + "/velodyne/" 
-//                         << std::setfill('0') << std::setw(6) << line_num << ".bin";
-//         std::vector<float> lidar_data = read_lidar_data(lidar_data_path.str());
-//         std::cout << "totally " << lidar_data.size() / 4.0 << " points in this lidar frame \n";
-
-//         std::vector<Eigen::Vector3d> lidar_points;
-//         std::vector<float> lidar_intensities;
-//         pcl::PointCloud<pcl::PointXYZI> laser_cloud;
-//         for (std::size_t i = 0; i < lidar_data.size(); i += 4)
-//         {
-//             lidar_points.emplace_back(lidar_data[i], lidar_data[i+1], lidar_data[i+2]);
-//             lidar_intensities.push_back(lidar_data[i+3]);
-
-//             pcl::PointXYZI point;
-//             point.x = lidar_data[i];
-//             point.y = lidar_data[i + 1];
-//             point.z = lidar_data[i + 2];
-//             point.intensity = lidar_data[i + 3];
-//             laser_cloud.push_back(point);
-//         }
 
 int main(){
 
