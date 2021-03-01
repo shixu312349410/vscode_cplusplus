2b0b2e1663d352c531f7f7d78b94c0748faa1bf7
xshian <312349410@qq.com>
2021-03-01 13:29:57 +0800

    update kd tree.cpp

---
+++ data_structure&algorithms/DataStructure&Algorithms/11_advanced_search_tree/src/main.cpp
@@ -0,0 +1,7 @@
+#include"kd-tree_construction.h"
+
+
+int main(){
+
+    return 0;
+}
---
+++ data_structure&algorithms/DataStructure&Algorithms/11_advanced_search_tree/src/partition.cpp
@@ -0,0 +1,20 @@
+#include"partition.h"
+#include"kd-tree_construction.h"
+
+/***************************************
+
+****************************************/
+void partition(int dir,int root,int tempA[],int tempB[]){  //dir:0 for X, 1 for Y,2 for Z
+
+    if(dir == 0){ 
+        
+
+        for(int i = 0; i<15;i++){
+
+           data_x[root] data[indices_y[i]][0]
+
+        }
+
+    }
+
+}
