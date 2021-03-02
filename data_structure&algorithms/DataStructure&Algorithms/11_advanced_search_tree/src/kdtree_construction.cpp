86c3008064f5289cbc36b7f12492d1d95252c588
xshian <312349410@qq.com>
2021-03-02 21:51:48 +0800

    modify kdtree_construction.cpp

--- data_structure&algorithms/DataStructure&Algorithms/11_advanced_search_tree/src/kdtree_construction.cpp
+++ data_structure&algorithms/DataStructure&Algorithms/11_advanced_search_tree/src/kdtree_construction.cpp
@@ -1,93 +1,132 @@
-// #include<bits/stdc++.h>
-// #include"mergesort.h"
-// #include"kdtree_construction.h"
-// using namespace std;
+#include<bits/stdc++.h>
+#include"mergesort.h"
+#include"kdtree_construction.h"
+#include"partition.h"
+using namespace std;
 
-// int main(){
+int main(){
 
 
-//     Merge_sort(data_x, indices_x,15);
-//     Merge_sort(data_y, indices_y,15);
-//     Merge_sort(data_z, indices_z,15);
+    Merge_sort(data_x, indices_x,15);
+    Merge_sort(data_y, indices_y,15);
+    Merge_sort(data_z, indices_z,15);
 
-//     for(int i =0;i<15;i++){
-//         cout<<indices_x[i]<<' ';
-//     }
-//     cout<<endl;
+    for(int i =0;i<15;i++){
+        cout<<indices_x[i]<<' ';
+    }
+    cout<<endl;
 
-//     for(int i =0;i<15;i++){
-//         cout<<indices_y[i]<<' ';
-//     }
-//     cout<<endl;
+    for(int i =0;i<15;i++){
+        cout<<indices_y[i]<<' ';
+    }
+    cout<<endl;
 
-//     for(int i =0;i<15;i++){
-//         cout<<indices_z[i]<<' ';
-//     }
-//     cout<<endl;
+    for(int i =0;i<15;i++){
+        cout<<indices_z[i]<<' ';
+    }
+    cout<<endl;
     
+    //迭代构造kd-tree
+    int dir = 0;
+    int start = 0;
+    int end = 14;
+    int parent = -1;
+    int rd = 0;
+    frame temp;
+    frame fm = {dir,start,end,parent,rd}; //区段[start,end]的父节点parent(在静态树表中的序号),rd代表从哪个递归规则处返回
+    stack<frame> st;
+    st.push(fm);
+    int root = -1;
 
-//     //迭代构造kd-tree
-//     int start = 0;
-//     int end = 14;
-//     int parent = -1;
-//     int rd = 0;
-//     frame temp;
-//     frame fm = {start,end,parent,rd}; //区段[start,end]的父节点parent,rd代表从哪个递归规则处返回
-//     stack<frame> st;
-//     st.push(fm);
-//     int root = -1;
+    while(!st.empty()){
 
-//     while(!st.empty()){
+        while(start<=end){  //处理栈顶元素，判断是否满足递归出口条件
 
-//         while(start<=end){   //处理栈顶元素，判断是否满足递归出口条件
+            root = st.top().start+(st.top().end-st.top().start)/2;
 
-//             root = (st.top().start+st.top().end)/2;
-//             tree[root].parent = st.top().parent;
-//             // if((st.top().parent != -1) && st.top().rd == 1){
-//             //     tree[st.top().parent].left = root;
-//             // }
-//             start = st.top().start;
-//             end = root - 1;
-//             //if(start<=end){
-//             parent = root;
-//             rd = 1;
-//             fm = {start,end,parent,rd};
-//             st.push(fm);
-//             //}
+            dir = st.top().dir;
+            partition(dir,st.top().start,st.top().end,indices_x,indices_y,indices_z);
+
+            if(dir == 0){
+                tree[indices_x[root]].parent = parent;
+                parent = indices_x[root]; 
+            }
+            else if(dir == 1){
+                tree[indices_y[root]].parent = parent;
+                parent = indices_y[root];
+            }
+            else {
+                tree[indices_z[root]].parent = parent;
+                parent = indices_z[root];
+            }
             
-//             // else {
-//             //     fm = st.top();
-//             //     st.pop();
+            // if((st.top().parent != -1) && st.top().rd == 1){
+            //     tree[st.top().parent].left = root;
+            // }
+            
+            start = st.top().start;
+            end = root - 1;
+            // if(start<=end){
+            //     partition(dir,start,end,indices_x,indices_y,indices_z);
+            // }
+            //if(start<=end){
+            dir = (dir+1)%3;
+            //parent = root;
+            rd = 1;
+            fm = {dir,start,end,parent,rd};
+            st.push(fm);
+            //}
+            
+            // else {
+            //     fm = st.top();
+            //     st.pop();
 
-//             // }
-//         }
+            // }
+        }
 
-//         while(!st.empty()){    //返回处理
-//             fm = st.top();
-//             st.pop();
-//             if(fm.rd == 0)    //从根节点返回
-//                 return true;
+        while(!st.empty()){   //返回处理
+            fm = st.top();
+            st.pop();
+            if(fm.rd == 0)    //从根节点返回
+                return true;
 
-//             if(fm.rd == 1){   //从左子树返回
-//                 // temp = st.top(); 
-//                 start = temp.start = fm.parent + 1;
-//                 end = temp.end = st.top().end;
-//                 temp.parent = fm.parent;
-//                 temp.rd = 2;
-//                 tree[fm.parent].left = fm.start+(fm.end-fm.start)/2;
-//                 st.push(temp);
-//                 break;
-//             }
-
-//             if(fm.rd == 2){  //从右子树返回
+            if(fm.rd == 1){   //从左子树返回
+                // temp = st.top(); 
+                start = temp.start = fm.end + 2;
+                end = temp.end = st.top().end;
+                temp.parent = fm.parent;
+                temp.rd = 2;
+                if(fm.dir == 0){
+                    tree[fm.parent].left = indices_x[fm.start+(fm.end-fm.start)/2];
+                }
+                else if(fm.dir == 1){
+                    tree[fm.parent].left = indices_y[fm.start+(fm.end-fm.start)/2];
+                }
+                else{
+                     tree[fm.parent].left = indices_z[fm.start+(fm.end-fm.start)/2];
+                }
                 
-//                 tree[fm.parent].right = fm.start+(fm.end-fm.start)/2;
+                st.push(temp);
+                break;
+            }
+
+            if(fm.rd == 2){  //从右子树返回
+                
+                if(fm.dir == 0){
+                    tree[fm.parent].right = indices_x[fm.start+(fm.end-fm.start)/2];
+                }
+                else if(fm.dir == 1){
+                    tree[fm.parent].right = indices_y[fm.start+(fm.end-fm.start)/2];
+                }
+                else{
+                     tree[fm.parent].right = indices_z[fm.start+(fm.end-fm.start)/2];
+                }
            
-//             }
+            }
 
-//         }
+        }
 
-//     }
+    }
     
-//     return 0;
-// }
+    return 0;
+}
--- data_structure&algorithms/DataStructure&Algorithms/11_advanced_search_tree/src/partition.cpp
+++ data_structure&algorithms/DataStructure&Algorithms/11_advanced_search_tree/src/partition.cpp
@@ -8,103 +8,173 @@ using namespace std;
  * 
  * 
 ****************************************/
-void partition(int dir,int start,int end,int tempA[],int tempB[]){ //dir:0 for X, 1 for Y,2 for Z
+void partition(int dir,int start,int end,int temp_indices_x[],int temp_indices_y[],int temp_indices_z[]){ //dir:0 for X, 1 for Y,2 for Z
 
     int num  = end-start+1;
     int root = start+(end-start)/2;
     // int ind1 = 0;
     // int ind2 = root;
-    if(dir == 0){ //X direction
-
+    if(dir == 0){ //select X as main direction, resort Y and Z direction
+        //resort Y direction
         for(int i = start,ind1 = start,ind2 = root+1; i<=end;i++){
             if(indices_y[i] == indices_x[root]){
                 continue;
             }
 
             if(data[indices_y[i]][0] < data[indices_x[root]][0]){
-                tempA[ind1++] = indices_y[i];
+                temp_indices_y[ind1++] = indices_y[i];
             }
             else{
-                tempA[ind2++] = indices_y[i];
+                temp_indices_y[ind2++] = indices_y[i];
             }
         }
 
+        for(int i = start;i<=end;i++){
+            indices_y[i] = temp_indices_y[i];
+        }
+
+        //resort Z direction
         for(int i = start,ind1 = start,ind2 = root+1; i<=end;i++){
             if(indices_z[i] == indices_x[root])
                 continue;
             
             if(data[indices_z[i]][0] < data[indices_x[root]][0]){
-                tempB[ind1++] = indices_z[i];
+                temp_indices_z[ind1++] = indices_z[i];
             }
             else{
-                tempB[ind2++] = indices_z[i];
+                temp_indices_z[ind2++] = indices_z[i];
             }
+        }
+
+        for(int i = start;i<=end;i++){
+            indices_z[i] = temp_indices_z[i];
         }
     }
 
-    if(dir == 1){ //Y direction
-
-        for(int i = 0,ind1 = 0,ind2 = root+1; i<num;i++){
+    if(dir == 1){  //select Y direction as main direction, resort X and Z direction
+        
+        //resort X dierection
+        for(int i = start,ind1 = start,ind2 = root+1; i<=end;i++){
             if(indices_x[i] == indices_y[root]){
                 continue;
             }
 
-            if(data[indices_x[i]][0] < data[tempA[root]][0]){
-                tempA[ind1++] = indices_x[i];
+            if(data[indices_x[i]][0] < data[indices_y[root]][0]){
+                temp_indices_x[ind1++] = indices_x[i];
             }
             else{
-                tempA[ind2++] = indices_x[i];
+                temp_indices_x[ind2++] = indices_x[i];
             }
         }
 
+        for(int i = start;i<=end;i++){
+            indices_x[i] = temp_indices_x[i];
+        }
+        
+        //resort Z direction
         for(int i = 0,ind1 = 0,ind2 = root+1; i<num;i++){
-            if(indices_y[i] == indices_x[root])
+            if(indices_z[i] == indices_y[root])
                 continue;
             
-            if(data[indices_z[i]][0] < data[indices_x[root]][0]){
-                tempB[ind1++] = indices_z[i];
+            if(data[indices_z[i]][0] < data[indices_y[root]][0]){
+                temp_indices_z[ind1++] = indices_z[i];
             }
             else{
-                tempB[ind2++] = indices_z[i];
+                temp_indices_z[ind2++] = indices_z[i];
             }
+        }
+
+        for(int i = start;i<=end;i++){
+            indices_z[i] = temp_indices_z[i];
+        }
+
+    }
+
+    if(dir == 2){ //select Z direction as main direction, resort X and Y direction
+        
+        //resort X dierection
+        for(int i = start,ind1 = start,ind2 = root+1; i<=end;i++){
+            if(indices_x[i] == indices_z[root]){
+                continue;
+            }
+
+            if(data[indices_x[i]][0] < data[indices_z[root]][0]){
+                temp_indices_x[ind1++] = indices_x[i];
+            }
+            else{
+                temp_indices_x[ind2++] = indices_x[i];
+            }
+        }
+
+        for(int i = start;i<=end;i++){
+            indices_x[i] = temp_indices_x[i];
+        }
+        
+        //resort Y direction
+        for(int i = 0,ind1 = 0,ind2 = root+1; i<num;i++){
+            if(indices_y[i] == indices_z[root])
+                continue;
+            
+            if(data[indices_y[i]][0] < data[indices_z[root]][0]){
+                temp_indices_y[ind1++] = indices_y[i];
+            }
+            else{
+                temp_indices_y[ind2++] = indices_y[i];
+            }
+        }
+
+        for(int i = start;i<=end;i++){
+            indices_y[i] = temp_indices_y[i];
         }
 
     }
 
 }
 
-int main(){
-    // int tempA[15];
-    // int tempB[15];
+// int main(){
+//     // int tempA[15];
+//     // int tempB[15];
 
-    Merge_sort(data_x, indices_x,15);
-    Merge_sort(data_y, indices_y,15);
-    Merge_sort(data_z, indices_z,15);
+//     Merge_sort(data_x, indices_x,15);
+//     Merge_sort(data_y, indices_y,15);
+//     Merge_sort(data_z, indices_z,15);
 
-    partition(0,0,14,tempA,tempB);
+//     partition(0,0,14,temp_indices_x,temp_indices_y,temp_indices_z);
+   
+//     cout<<"hello"<<endl;
 
-    cout<<"hello"<<endl;
+//     for(int i = 0;i<15;i++){
+//         cout<<indices_y[i]<<" ";
+//     }
+//     cout<<endl;
 
-    for(int i = 0;i<15;i++){
-        cout<<indices_y[i]<<" ";
-    }
-    cout<<endl;
+//     // for(int i = 0;i<15;i++){
+//     //     cout<<indices_y[i]<<" ";
+//     // }
+//     // cout<<endl;
+//     // cout<<endl;
 
-    for(int i = 0;i<15;i++){
-        cout<<tempA[i]<<" ";
-    }
-    cout<<endl;
-    cout<<endl;
+//     for(int i = 0;i<15;i++){
+//         cout<<indices_z[i]<<" ";
+//     }
+//     cout<<endl;
 
-    for(int i = 0;i<15;i++){
-        cout<<indices_z[i]<<" ";
-    }
-    cout<<endl;
+//     // for(int i = 0;i<15;i++){
+//     //     cout<<tempB[i]<<" ";
+//     // }
+//     // cout<<endl;
 
-    for(int i = 0;i<15;i++){
-        cout<<tempB[i]<<" ";
-    }
-    cout<<endl
+//     partition(1,0,6,temp_indices_x,temp_indices_y,temp_indices_z);
+//     for(int i = 0;i<7;i++){
+//         cout<<indices_y[i]<<" ";
+//     }
+//     cout<<endl;
 
-    return 0;
-}
+//     partition(1,8,14,temp_indices_x,temp_indices_y,temp_indices_z);
+//     for(int i = 8;i<=14;i++){
+//         cout<<indices_y[i]<<" ";
+//     }
+//     cout<<endl;
+
+//     return 0;
+// }
